15e54a26ae07f5194ad6b60bb52268995b3ffafd
Jess Delgado Perez <jessdelgadoperez@drata.com>
2025-05-02 10:04:04 -0500

    feat: add conditional listening

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3005;
-  await app.listen(port, '0.0.0.0', () => {
-    console.log(`App listening at http://0.0.0.0:${port}`);
-  });
+  const port = process.env.APP_PORT || 3005;
+  if (process.env.NODE_CONFIG_ENV === 'dev') {
+    await app.listen(port);
+    console.log(`App listening at http://localhost:${port}`);
+  } else {
+    await app.listen(port, '0.0.0.0', () => {
+      console.log(`App listening at http://0.0.0.0:${port}`);
+    });
+  }
 }
 bootstrap();
