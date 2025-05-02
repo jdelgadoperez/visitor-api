b9ef00d162cf43dccd133ca1abe5ea50554fdb1e
Jess Delgado Perez <jessdelgadoperez@drata.com>
2025-05-02 09:19:25 -0500

    feat: expand listening

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3005);
+  const port = process.env.PORT || 3005;
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`App listening at http://0.0.0.0:${port}`);
+  });
 }
 bootstrap();
