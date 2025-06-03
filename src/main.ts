e4ad25d04a8d564ba5630b76d7a42d52f0d1745b
Jess Delgado Perez <jessdelgadoperez@drata.com>
2025-06-03 17:57:49 -0500

    chore(pkg): add env config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
